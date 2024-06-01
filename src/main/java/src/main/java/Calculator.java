42cdd8ea128b7289172e2113b190ff8afd2663d2
Rzv888 <127009592+Rzv888@users.noreply.github.com>
2024-06-01 10:22:00 +0700

    Update Calculator.java

--- src/main/java/src/main/java/Calculator.java
+++ src/main/java/src/main/java/Calculator.java
@@ -3,13 +3,7 @@ package src.main.java;
 
 public class Calculator {
 
-    public int add(int a, int b) {
-        return a + b;
-    }
-
-    public int subtract(int a, int b) {
-        return a - b;
-    }
+    
 
     public int multiply(int a, int b) {
         return a * b;
